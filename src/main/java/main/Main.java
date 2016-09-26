86e027dbd1dd9223415dd8ab716b40d6fd64c777
v1sar <v1sar@mail.ru>
2016-09-26 16:07:12 +0300

    added httpd, minor fixes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,15 +13,21 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    private static final int PORT = 8080;
+    private static int PORT = 80;
     private static final int QUEUE = 100;
     private static int POOL_SIZE = 4;
 
     public static void main(String[] args) throws IOException{
         CmdLine parser = new CmdLine(args);
         String ROOTDIR = parser.getValue("r");
-        POOL_SIZE = Integer.parseInt(parser.getValue("c"));
-
+        try {
+            POOL_SIZE = Integer.parseInt(parser.getValue("c"));
+        }
+        catch (Exception e) {}
+        try {
+            PORT = Integer.parseInt(parser.getValue("p"));
+        }
+        catch (Exception e) {}
         FileSystem.DOCUMENT_ROOT = ROOTDIR;
         ServerSocket serverSocket = new ServerSocket(PORT,QUEUE);
         ForkJoinPool pool = new ForkJoinPool(POOL_SIZE*2 + 1);
@@ -30,7 +36,6 @@ public class Main {
                 .append("CPU count is: "+POOL_SIZE+'\n')
                 .append("ROOTDIR is: "+ROOTDIR+'\n')
                 .toString());
-
         while(true){
             Socket socket = serverSocket.accept();
             pool.execute(new Task(socket));
